929010dd74fcf6182b0ef14423d3813d40fd7c49
Patrick <patrick.lloyd@theproducebox.com>
2020-10-15 22:15:28 -0400

    updating trails api and creating test html

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var results = 8
-var radius = 50
+var radius = 25
+var trailsListEl = $('.trails-list');
+var trailsInfoEl = $('.trails-info');
 
 
 var locale = function (callback) {
@@ -13,14 +15,14 @@ var locale = function (callback) {
 	});
 };
 
-var getPosition = function () {
+
+var getTrails = function () {
+
 	locale(function (lat, lon) {
-	
+
 		var trailUrl = "https://trailapi-trailapi.p.rapidapi.com/trails/explore/?page=3&per_page= " + results + "&radius=" + radius + "&lat=" + lat + "&lon=" + lon
 
 		$.ajax({
-				async: true,
-				crossDomain: true,
 				url: trailUrl,
 				headers: {
 					'x-rapidapi-host': 'trailapi-trailapi.p.rapidapi.com',
@@ -30,11 +32,67 @@ var getPosition = function () {
 			})
 
 			.then(function (response) {
+				console.log(response)
 				var results = response.data;
 
 				results.forEach((data) => {
-					console.log('name :', data.name, ' ID: ', data.id);
+					// console.log('name :', data.name, ' ID: ', data.id);
+
+					var trailName = $('<div>')
+						.text(data.name)
+
+
+					var trailInfoButton = $('<button>')
+						.text('more info')
+						.attr('trail-id', data.id)
+						.addClass('info');
+
+					trailsListEl.append(trailName, trailInfoButton);
+
+
 				});
+
+				$('.info').click(function () {
+					$( ".trail-info" ).empty();
+					var trailID = $(this).attr('trail-id');
+					trailInfo(trailID)
+				})
+
 			});
+
+
 	});
 };
+
+
+var trailInfo = function (trailID) {
+
+	var trailInfo = "https://rapidapi.p.rapidapi.com/trails/" + trailID
+	$.ajax({
+			url: trailInfo,
+			headers: {
+				'x-rapidapi-host': 'trailapi-trailapi.p.rapidapi.com',
+				'x-rapidapi-key': 'aa1f2dda11msh22ea2d650da7d5fp193f76jsn94dfb4977b89',
+			},
+			method: 'GET',
+		})
+		.then(function (response) {
+			// console.log(response)
+			var results = response.data;
+
+			results.forEach((data) => {
+				// console.log('name :', data.name, ' ID: ', data.id);
+
+				var trailName = $('<div>').text(data.description)
+				.addClass('trail-info');
+				trailsInfoEl.append(trailName);
+
+
+			});
+
+		});
+
+
+
+
+}
